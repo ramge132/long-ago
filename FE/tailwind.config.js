c82a41fc9c1ec9eaf1e14989775be54ceaae8970
홍석진 <seokbangguri@kakao.com>
2025-01-22 17:42:54 +0900

    fix: 폰트 깨지는 이슈 해결

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        kcc: [""],
         hs: ["HS유지체", ...defaultTheme.fontFamily.sans],
         makgeolli: ["MAKGEOLLI", ...defaultTheme.fontFamily.sans],
       },
