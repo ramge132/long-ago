00bf39f9ac382029553eddadc60c84b71cd2df55
홍석진 <seokbangguri@kakao.com>
2025-01-31 12:06:26 +0900

    feat: 긴장감 바 추가

--- FE/src/components/index.js
+++ FE/src/components/index.js
@@ -17,3 +17,4 @@ export { default as InGameContent } from "./InGame/InGameContent.vue";
 // InGameContent
 export { default as ContentTimer } from "./InGame/InGameContent/ContentTimer.vue";
 export { default as ContentBook } from "./InGame/InGameContent/ContentBook.vue";
+export { default as ContentGuage } from "./InGame/InGameContent/ContentGuage.vue";
