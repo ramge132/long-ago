1a10934313e135f4dc42dc389b276dd2d9f7574e
이세중 <sejoong.biz@gmail.com>
2025-02-03 13:23:11 +0900

    test: BE

--- BE/src/main/java/com/example/b101/EreGgApplication.java
+++ BE/src/main/java/com/example/b101/EreGgApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.TimeZone;
 
