1e38c12f87233dc46173043fb46e6cc6e874dd80
이세중 <sejoong.biz@gmail.com>
2025-02-05 22:31:28 +0900

    test: env_main

--- BE/src/main/java/com/example/b101/EreGgApplication.java
+++ BE/src/main/java/com/example/b101/EreGgApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.TimeZone;
 
@@ -16,13 +15,6 @@ public class EreGgApplication {
 
     public static void main(String[] args) {
 
-        // .env 파일 로드
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
-        System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
 
         SpringApplication.run(EreGgApplication.class, args);
     }
