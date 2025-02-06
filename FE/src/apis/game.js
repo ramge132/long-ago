fe2cbf9fa641b1661bd3805fa5901fe14c69c9db
홍석진 <seokbangguri@kakao.com>
2025-02-06 13:50:46 +0900

    feat: 게임 관련 api 추가가

--- FE/src/apis/game.js
+++ FE/src/apis/game.js
@@ -2,9 +2,51 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_MAIN_API_SERVER_URL, // API의 기본 URL을 설정하세요.
-    headers: {
-      "Content-Type": "application/json",
-    },
-    withCredentials: false,
-  });
+  baseURL: import.meta.env.VITE_MAIN_API_SERVER_URL, // API의 기본 URL을 설정하세요.
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: false,
+});
+
+// 게임 api 부분
+
+// 방 생성 (방장인 경우만 요청하면 됩니둥~)
+export const createGame = async (data) => {
+  try {
+    const response = await apiClient.post(import.meta.env.VITE_GAME, data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// 방 입장 (방장 외 참가자들만 요청하면 됩니둥둥~~)
+export const enterGame = async (data) => {
+  try {
+    const response = await apiClient.get(import.meta.env.VITE_GAME, data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// 방 삭제 (방장만 게임 종료 시 삭제하면 됩니둥둥둥~)
+export const deleteGame = async (data) => {
+  try {
+    const response = await apiClient.delete(import.meta.env.VITE_GAME, data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// 엔딩카드 리롤하는 겁니둥둥둥둥둥둥
+export const endingCardReroll = async (data) => {
+  try {
+    const response = await apiClient.patch(import.meta.env.VITE_GAME + import.meta.env.VITE_GAME_SHUFFLE, data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
