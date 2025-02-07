2ae96f9ad5c21ce49f13463ed42c5c51fed8810f
hphp <jmn9798@naver.com>
2025-02-07 14:25:59 +0900

    feat: 이미지 생성 로직

--- BE/src/main/java/com/example/b101/dto/GenerateSceneRequest.java
+++ BE/src/main/java/com/example/b101/dto/GenerateSceneRequest.java
@@ -11,13 +11,7 @@ public class GenerateSceneRequest {
     //작화 스타일
     String drawingStyle;
 
-    //요약된 스토리 (직전 Scene의 데이터)
-    String storySummary;
-
     //사용자 프롬포트
     String userPrompt;
-
-    //AI가 생성한 프롬포트
-    String imagePrompt;
 
 }
--- BE/src/main/java/com/example/b101/service/UserService.java
+++ BE/src/main/java/com/example/b101/service/UserService.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UserService {
