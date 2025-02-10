ba23a769a6c49d4ed8acc9ec37a8be50bebe82a4
홍석진 <seokbangguri@kakao.com>
2025-02-10 15:36:01 +0900

    refactor: 이미지 export 방식 변경

--- FE/src/assets/images/profiles/index.js
+++ FE/src/assets/images/profiles/index.js
@@ -1,43 +1,87 @@
+import cat_1 from "@/assets/images/profiles/cat_1.svg"
+import cat_2 from "@/assets/images/profiles/cat_2.svg"
+import cat_4 from "@/assets/images/profiles/cat_4.svg"
+import cat_5 from "@/assets/images/profiles/cat_5.svg"
+import cat_6 from "@/assets/images/profiles/cat_6.svg"
+import dog_1 from "@/assets/images/profiles/dog_1.svg"
+import dog_2 from "@/assets/images/profiles/dog_2.svg"
+import dog_3 from "@/assets/images/profiles/dog_3.svg"
+import dog_4 from "@/assets/images/profiles/dog_4.svg"
+import dog_5 from "@/assets/images/profiles/dog_5.svg"
+import dog_6 from "@/assets/images/profiles/dog_6.svg"
+import cattle_1 from "@/assets/images/profiles/cattle_1.svg"
+import cattle_2 from "@/assets/images/profiles/cattle_2.svg"
+import cattle_3 from "@/assets/images/profiles/cattle_3.svg"
+import cattle_4 from "@/assets/images/profiles/cattle_4.svg"
+import cow_1 from "@/assets/images/profiles/cow_1.svg"
+import chicken_1 from "@/assets/images/profiles/chicken_1.svg"
+import dragon_1 from "@/assets/images/profiles/dragon_1.svg"
+import dragon_2 from "@/assets/images/profiles/dragon_2.svg"
+import dragon_3 from "@/assets/images/profiles/dragon_3.svg"
+import dragon_4 from "@/assets/images/profiles/dragon_4.svg"
+import horse_1 from "@/assets/images/profiles/horse_1.svg"
+import monkey_1 from "@/assets/images/profiles/monkey_1.svg"
+import monkey_2 from "@/assets/images/profiles/monkey_2.svg"
+import pig_1 from "@/assets/images/profiles/pig_1.svg"
+import pig_2 from "@/assets/images/profiles/pig_2.svg"
+import pig_4 from "@/assets/images/profiles/pig_4.svg"
+import rabbit_1 from "@/assets/images/profiles/rabbit_1.svg"
+import rabbit_2 from "@/assets/images/profiles/rabbit_2.svg"
+import rabbit_3 from "@/assets/images/profiles/rabbit_3.svg"
+import rabbit_4 from "@/assets/images/profiles/rabbit_4.svg"
+import rat_1 from "@/assets/images/profiles/rat_1.svg"
+import rat_2 from "@/assets/images/profiles/rat_2.svg"
+import rat_3 from "@/assets/images/profiles/rat_3.svg"
+import rat_4 from "@/assets/images/profiles/rat_4.svg"
+import rat_5 from "@/assets/images/profiles/rat_5.svg"
+import sheep_1 from "@/assets/images/profiles/sheep_1.svg"
+import sheep_2 from "@/assets/images/profiles/sheep_2.svg"
+import sheep_3 from "@/assets/images/profiles/sheep_3.svg"
+import tiger_1 from "@/assets/images/profiles/tiger_1.svg"
+import tiger_2 from "@/assets/images/profiles/tiger_2.svg"
+
+
+
 export default {
- cat_1: "/src/assets/images/profiles/cat_1.svg",
- cat_2: "/src/assets/images/profiles/cat_2.svg",
- cat_4: "/src/assets/images/profiles/cat_4.svg",
- cat_5: "/src/assets/images/profiles/cat_5.svg",
- cat_6: "/src/assets/images/profiles/cat_6.svg",
- dog_1: "/src/assets/images/profiles/dog_1.svg",
- dog_2: "/src/assets/images/profiles/dog_2.svg",
- dog_3: "/src/assets/images/profiles/dog_3.svg",
- dog_4: "/src/assets/images/profiles/dog_4.svg",
- dog_5: "/src/assets/images/profiles/dog_5.svg",
- dog_6: "/src/assets/images/profiles/dog_6.svg",
- cattle_1: "/src/assets/images/profiles/cattle_1.svg",
- cattle_2: "/src/assets/images/profiles/cattle_2.svg",    
- cattle_3: "/src/assets/images/profiles/cattle_3.svg",    
- cattle_4: "/src/assets/images/profiles/cattle_4.svg",    
- cow_1: "/src/assets/images/profiles/cow_1.svg",
- chicken_1: "/src/assets/images/profiles/chicken_1.svg",
- dragon_1: "/src/assets/images/profiles/dragon_1.svg",    
- dragon_2: "/src/assets/images/profiles/dragon_2.svg",    
- dragon_3: "/src/assets/images/profiles/dragon_3.svg",    
- dragon_4: "/src/assets/images/profiles/dragon_4.svg",    
- horse_1: "/src/assets/images/profiles/horse_1.svg",    
- monkey_1: "/src/assets/images/profiles/monkey_1.svg",    
- monkey_2: "/src/assets/images/profiles/monkey_2.svg",    
- pig_1: "/src/assets/images/profiles/pig_1.svg",    
- pig_2: "/src/assets/images/profiles/pig_2.svg",    
- pig_4: "/src/assets/images/profiles/pig_4.svg",    
- rabbit_1: "/src/assets/images/profiles/rabbit_1.svg",    
- rabbit_2: "/src/assets/images/profiles/rabbit_2.svg",    
- rabbit_3: "/src/assets/images/profiles/rabbit_3.svg",    
- rabbit_4: "/src/assets/images/profiles/rabbit_4.svg",    
- rat_1: "/src/assets/images/profiles/rat_1.svg",    
- rat_2: "/src/assets/images/profiles/rat_2.svg",    
- rat_3: "/src/assets/images/profiles/rat_3.svg",    
- rat_4: "/src/assets/images/profiles/rat_4.svg",    
- rat_5: "/src/assets/images/profiles/rat_5.svg",    
- sheep_1: "/src/assets/images/profiles/sheep_1.svg",    
- sheep_2: "/src/assets/images/profiles/sheep_2.svg",    
- sheep_3: "/src/assets/images/profiles/sheep_3.svg",    
- tiger_1: "/src/assets/images/profiles/tiger_1.svg",    
- tiger_2: "/src/assets/images/profiles/tiger_2.svg",    
+ cat_1,
+ cat_2,
+ cat_4,
+ cat_5,
+ cat_6,
+ dog_1,
+ dog_2,
+ dog_3,
+ dog_4,
+ dog_5,
+ dog_6,
+ cattle_1,
+ cattle_2,    
+ cattle_3,    
+ cattle_4,    
+ cow_1,
+ chicken_1,
+ dragon_1,    
+ dragon_2,    
+ dragon_3,    
+ dragon_4,    
+ horse_1,    
+ monkey_1,    
+ monkey_2,    
+ pig_1,    
+ pig_2,    
+ pig_4,    
+ rabbit_1,    
+ rabbit_2,    
+ rabbit_3,    
+ rabbit_4,    
+ rat_1,    
+ rat_2,    
+ rat_3,    
+ rat_4,    
+ rat_5,    
+ sheep_1,    
+ sheep_2,    
+ sheep_3,    
+ tiger_1,    
+ tiger_2,    
 }
