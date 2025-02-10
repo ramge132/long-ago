27b581a62f309e1ccd9672f3359dd54107f52125
seokbangguri <seokbangguri@kakao.com>
2025-02-10 21:47:21 +0900

    test: 턴서버 테스트

--- FE/src/views/GameView.vue
+++ FE/src/views/GameView.vue
@@ -489,7 +489,7 @@ const initializePeer = () => {
           iceServers: [
             { urls: "stun:stun.l.google.com:19302" }, // 예제 STUN 서버
             {
-              urls: "turn:your.turn.server:3478",   // 턴서버 제작완료하면 바꿔야함
+              urls: "turn:i12b101.p.ssafy.io:3478",   // 턴서버 제작완료하면 바꿔야함
               username: import.meta.env.VITE_TURN_ID,             // docker 환경변수 참고
               credential: import.meta.env.VITE_TURN_PW          // docker 환경변수 참고
             }
