0b7f70a6ca37cb9b93ec3d697d5f53af50c9e15e
홍석진 <seokbangguri@kakao.com>
2025-02-14 15:21:33 +0900

    feat: 인게임 화면에서 키보드 입력 감지 후 입력창 포커스

--- FE/src/components/InGame/InGameControl.vue
+++ FE/src/components/InGame/InGameControl.vue
@@ -189,7 +189,10 @@ const chatMode = ref([
 const currChatModeIdx = ref(0);
 
 window.addEventListener("keydown", (e) => {
-  if (e.ctrlKey) changeMode();
+  if (e.key === "Tab"){
+    changeMode();
+    e.preventDefault();
+  }
 });
 
 const changeMode = () => {
@@ -247,6 +250,10 @@ onMounted(() => {
 
   });
 });
+
+onkeydown = () => {
+  chatRefs.value[currChatModeIdx.value].focus();
+};
 </script>
 
 <style scoped>
