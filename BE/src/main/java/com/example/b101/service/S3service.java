8ec70c39d5549bf34e8762ca5035a22f0f0a2b93
이세중 <sejoong.biz@gmail.com>
2025-02-14 17:54:21 +0900

    fix: 환경 변수

--- BE/src/main/java/com/example/b101/service/S3service.java
+++ BE/src/main/java/com/example/b101/service/S3service.java
@@ -136,7 +136,7 @@ public class S3service {
             log.info(awsConfig.getAccessKey());
             log.info(awsConfig.getSecretKey());
             log.info(objectKey);
-            log.info("내 로그 여기있어요!!!!");
+            log.info("내 로그 여기있어 요!!!!");
             // S3 업로드 객체 생성
             GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                     .bucket(awsConfig.getBucketName())
