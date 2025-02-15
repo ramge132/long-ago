179cbf3dde6f4c535afd59b90d6100a32a780de0
JPhong <hsc3757@gmail.com>
2025-02-15 15:53:34 +0900

    style: turn server 환경변수 재적용

--- FE/src/views/GameView.vue
+++ FE/src/views/GameView.vue
@@ -676,11 +676,11 @@ const initializePeer = () => {
       peer.value = new Peer({
         config: {
           iceServers: [
-            { urls: "stun:stun.l.google.com:19302" }, // 예제 STUN 서버
+            // { urls: "stun:stun.l.google.com:19302" }, // 예제 STUN 서버
             {
               urls: "turn:i12b101.p.ssafy.io:3478",   // 턴서버 제작완료하면 바꿔야함
-              username: "",              // docker 환경변수 참고
-              credential: ""         // docker 환경변수 참고
+              username: import.meta.env.VITE_TURN_ID,              // docker 환경변수 참고
+              credential: import.meta.env.VITE_TURN_PW,         // docker 환경변수 참고
             }
           ]
         }
