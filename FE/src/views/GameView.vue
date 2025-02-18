b0fff81311660a72a86558330ffea0c3a863dbfd
seokbangguri <seokbangguri@kakao.com>
2025-02-18 21:34:49 +0900

    fix: 6번째 참가자 짤리는 이슈

--- FE/src/views/GameView.vue
+++ FE/src/views/GameView.vue
@@ -206,7 +206,7 @@ const setupConnection = (conn) => {
   }, 5000);
 
   
-  if (participants.value.length >= maxParticipants) {
+  if (participants.value.length > maxParticipants) {
     conn.close();
     return;
   }
@@ -830,8 +830,6 @@ onMounted(async () => {
   } catch (error) {
     console.error("Peer initialization failed:", error);
   }
-
-  // checkPeerConnections();
 });
 
 // // 퇴장 관련
