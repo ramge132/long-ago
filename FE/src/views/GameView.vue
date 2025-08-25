888e075a0ef39b76b78eb1662766c66309ad3f2c
유태영 <yty96@naver.com>
2025-08-25 16:40:10 +0900

    modify: turnserver 하드코딩 부분 수정

--- FE/src/views/GameView.vue
+++ FE/src/views/GameView.vue
@@ -826,9 +826,9 @@ const initializePeer = () => {
           iceServers: [
             // { urls: "stun:stun.l.google.com:19302" }, // 예제 STUN 서버
             {
-              urls: "turn:i12b101.p.ssafy.io:3478",   // 턴서버 제작완료하면 바꿔야함
-              username: import.meta.env.VITE_TURN_ID,              // docker 환경변수 참고
-              credential: import.meta.env.VITE_TURN_PW,         // docker 환경변수 참고
+              urls: import.meta.env.VITE_TURN_SERVER_URL,
+              username: import.meta.env.VITE_TURN_ID,
+              credential: import.meta.env.VITE_TURN_PW,
             }
           ]
         }
