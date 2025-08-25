31f8ba7cc06d6771d33dbf9159c99d78edd3b40b
유태영 <yty96@naver.com>
2025-08-25 19:07:14 +0900

    modify: 명단 제거

--- FE/src/components/Presets/FooterBar.vue
+++ FE/src/components/Presets/FooterBar.vue
@@ -6,7 +6,7 @@
       class="w-full h-full rounded-lg bg-[#00000080] flex items-center justify-between px-4 text-white"
     >
       <div class="font-semibold" @click="emit('toggleTerms')">이용약관</div>
-      <div>B101 | 김동휘 | 김형표 | 유태영 | 이세중 | 홍석진 | 홍정표</div>
+      <div></div>
       <div class="font-semibold text-xs">&copy; 2025. LONG AGO All rights reserved.</div>
     </div>
   </div>
