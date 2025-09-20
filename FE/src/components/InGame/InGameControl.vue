d6ce7b4d4434926c05327cd4e7367d427f1113af
유태영 <yty96@naver.com>
2025-09-21 01:48:55 +0900

    간격4

--- FE/src/components/InGame/InGameControl.vue
+++ FE/src/components/InGame/InGameControl.vue
@@ -755,33 +755,33 @@ watch(() => message.value, (newValue) => {
 }
 
 .card4 > :nth-child(1){
-  transform: translateX(-75px);
+  transform: translateX(-50px);
 }
 
 .card4 > :nth-child(2){
-  transform: translateX(-25px);
+  transform: translateX(-15px);
 }
 .card4 > :nth-child(3){
-  transform: translateX(25px);
+  transform: translateX(15px);
 }
 .card4 > :nth-child(4){
-  transform: translateX(75px);
+  transform: translateX(50px);
 }
 
 .card3 > :nth-child(1){
-  transform: translateX(-40px);
+  transform: translateX(-25px);
 }
 .card3 > :nth-child(2){
   transform: translateX(0);
 }
 .card3 > :nth-child(3){
-  transform: translateX(40px);
+  transform: translateX(25px);
 }
 
 .card2 > :nth-child(1){
-  transform: translateX(-30px);
+  transform: translateX(-20px);
 }
 .card2 > :nth-child(2){
-  transform: translateX(30px);
+  transform: translateX(20px);
 }
 </style>
